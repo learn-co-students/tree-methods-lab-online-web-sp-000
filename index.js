0ae2ca87b91276cfcc0e4c4e0caf8b1ead9cd537
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-07 17:49:44 -0400

    updating solution to use ternary statement

--- index.js
+++ index.js
@@ -16,40 +16,22 @@ function findOrAdd(root, node) {
       if (root.left) {
         return findOrAdd(root.left, node);
       } else {
-        root.left = node;
+        return root.left = node;
       }
     } else {
       if (root.right) {
         return findOrAdd(root.right, node);
       } else {
-        root.right = node;
+        return root.right = node;
       }
     }
   }
 }
 
 function max(node) {
-  let leftMax = node.left ? max(node.left) : node
-  let rightMax = node.right ? max(node.right) : node
-
-  if (leftMax.data > node.data && leftMax.data > rightMax.data) {
-    return leftMax
-  } else if (rightMax.data > node.data && rightMax.data > leftMax.data) {
-    return rightMax
-  } else {
-    return node
-  }
+  return node.right ? max(node.right) : node
 }
 
 function min(node) {
-  let leftMin = node.left ? min(node.left) : node
-  let rightMin = node.right ? min(node.right) : node
-
-  if (leftMin.data < node.data && leftMin.data < rightMin.data) {
-    return leftMin
-  } else if (rightMin.data < node.data && rightMin.data < leftMin.data) {
-    return rightMin
-  } else {
-    return node
-  }
+  return node.left ? min(node.left) : node
 }
